026d582e6286967e7e6daaa98bbe3980c5f33ada
Fishphobiagg <dlauddn6211@icloud.com>
2023-04-21 09:26:53 +0900

    1234

---
+++ movies/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def actor_list(request):
+    pass
+
+def actor_detail(request, actor_pk):
+    pass
+
+def movie_list(request):
+    pass
+
+def movie_detail(request, movie_pk):
+    pass
+
+def review_list(request):
+    pass
+
+def review_detail(request, review_pk):
+    pass
+
+def create_review(request):
+    pass
